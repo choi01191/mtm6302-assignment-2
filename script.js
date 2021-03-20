5c1d7493ab80b0213691fb0a29d7fc0315420174
GoEun Choi <choi0119@algonquinlive.com>
2021-03-20 13:50:37 -0400

    draft version

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 
 const $gallery = document.getElementById('gallery')
 
+window.addEventListener('click', ()=>{
 
-for (let i =1; i <= 12; i++){
-   let image = document.createElement('img');
-   image.setAttribute('src', 'image/'+i+'.jpg');
-   document.querySelector("#gallery").appendChild(image)
-}
-funciton 
+})
+
+let $images = document.querySelectorAll('img')
+console.log($images)
+//
+   $gallery.addEventListener("click", event =>{
+      let alt =event.target.getAttribute('alt')
+      if(alt== null){alert('background is clicked')}
+     else { alert (alt+' is clicked')}
+   })
+
+//dbclick event
+
+const $button =document.getElementById('bt')
+$button.addEventListener('click',function(e){
+
+   for (let i =1; i <= 12; i++){
+      let image = document.createElement('img');
+      image.setAttribute('src', 'image/'+i+'.jpg');
+      image.setAttribute('alt',`image${i}`)
+      document.querySelector("#gallery").appendChild(image)
+   }
+})
+
+   $gallery.addEventListener('mouseover',function(e){
+      
+         e.target.style.width="70%"
+      })
+
+      $gallery.addEventListener('mouseout',function(e){
+
+         e.target.style.with="50%"
+      })
